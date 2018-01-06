2dc0c13a4cff34708f9309a5727963567b7232b9
Daniel Andrade <dca650@gmail.com>
2018-01-05 23:55:22 -0300

    Add CryptoNews Module

---
+++ modules/cryptoNews/index.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const config = require('./config');
+
+function get(callback) {
+  const API = 'https://newsapi.org/v2/';
+  const QUERY = 'everything';
+  const QUERYSTRING = `?sources=crypto-coins-news&apiKey=${process.env.NEWSAPI_TOKEN}`;
+
+  axios.get(`${API}/${QUERY}/${QUERYSTRING}`)
+    .then(function(response) {
+      const articles = response.data.articles;
+      let news = [];
+
+      for (var i = 0; i < 11; i++) {
+        news.push(articles[i].title);
+      }
+
+      callback(news);
+    });
+}
+
+module.exports = { get, config };
